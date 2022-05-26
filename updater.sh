c3d3951ed1b1c033d1334cd8e0fc54f392a5e8de
fernvenue <git@fernvenue.com>
2022-05-26 13:15:46 +0000

    Update updater.sh

--- updater.sh
+++ updater.sh
@@ -8,57 +8,21 @@ git config --global user.email "$GIT_MAIL_ADDRESS"
 git config --global user.name "fernvenue"
 git clone git@gitlab.com:fernvenue/chn-domains-list.git
 cd "./chn-domains-list"
-curl "https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/accelerated-domains.china.conf" -o "./accelerated-domains.china.conf"
-sed -i "/^#/d" "./accelerated-domains.china.conf"
-sed -i "s/[[:space:]]//g" "./accelerated-domains.china.conf"
-cp ./accelerated-domains.china.conf ./CHN.txt
-sed -i "s|server=/||g" ./CHN.txt
-sed -i "s|/114.114.114.114||g" ./CHN.txt
-cp ./accelerated-domains.china.conf ./CHN.conf
-sed -i "s|server=/|DOMAIN-SUFFIX,|g" ./CHN.conf
-sed -i "s|/114.114.114.114||g" ./CHN.conf
-cp ./accelerated-domains.china.conf ./CHN.yaml
-sed -i "s|server=/|  - '+.|g" ./CHN.yaml
-sed -i "s|/114.114.114.114|'|g" ./CHN.yaml
-sed -i "1s|^|payload:\n|" ./CHN.yaml
-cp ./accelerated-domains.china.conf ./CHN.agh
-sed -i "s|server=|[|g" ./CHN.agh
-sed -i "s|114.114.114.114|]tls://223.5.5.5|g" ./CHN.agh
-rm ./accelerated-domains.china.conf
-curl "https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/apple.china.conf" -o "./apple.china.conf"
-sed -i "/^#/d" "./apple.china.conf"
-sed -i "s/[[:space:]]//g" "./apple.china.conf"
-cp ./apple.china.conf ./CHN.AAPL.txt
-sed -i "s|server=/||g" ./CHN.AAPL.txt
-sed -i "s|/114.114.114.114||g" ./CHN.AAPL.txt
-cp ./apple.china.conf ./CHN.AAPL.conf
-sed -i "s|server=/|DOMAIN-SUFFIX,|g" ./CHN.AAPL.conf
-sed -i "s|/114.114.114.114||g" ./CHN.AAPL.conf
-cp ./apple.china.conf ./CHN.AAPL.yaml
-sed -i "s|server=/|  - '+.|g" ./CHN.AAPL.yaml
-sed -i "s|/114.114.114.114|'|g" ./CHN.AAPL.yaml
-sed -i "1s|^|payload:\n|" ./CHN.AAPL.yaml
-cp ./apple.china.conf ./CHN.AAPL.agh
-sed -i "s|server=|[|g" ./CHN.AAPL.agh
-sed -i "s|114.114.114.114|]tls://223.5.5.5|g" ./CHN.AAPL.agh
-rm ./apple.china.conf
-curl "https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/google.china.conf" -o "./google.china.conf"
-sed -i "/^#/d" "./google.china.conf"
-sed -i "s/[[:space:]]//g" "./google.china.conf"
-cp ./google.china.conf ./CHN.GOOG.txt
-sed -i "s|server=/||g" ./CHN.GOOG.txt
-sed -i "s|/114.114.114.114||g" ./CHN.GOOG.txt
-cp ./google.china.conf ./CHN.GOOG.conf
-sed -i "s|server=/|DOMAIN-SUFFIX,|g" ./CHN.GOOG.conf
-sed -i "s|/114.114.114.114||g" ./CHN.GOOG.conf
-cp ./google.china.conf ./CHN.GOOG.yaml
-sed -i "s|server=/|  - '+.|g" ./CHN.GOOG.yaml
-sed -i "s|/114.114.114.114|'|g" ./CHN.GOOG.yaml
-sed -i "1s|^|payload:\n|" ./CHN.GOOG.yaml
-cp ./google.china.conf ./CHN.GOOG.agh
-sed -i "s|server=|[|g" ./CHN.GOOG.agh
-sed -i "s|114.114.114.114|]tls://223.5.5.5|g" ./CHN.GOOG.agh
-rm ./google.china.conf
+curl -O "https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/accelerated-domains.china.conf" -O "https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/apple.china.conf" -O "https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/google.china.conf"
+sed -i "/^#/d" ./*.conf && sed -i "s/[[:space:]]//g" ./*.conf
+cp ./accelerated-domains.china.conf ./CHN.txt && cp ./apple.china.conf ./CHN.AAPL.txt && cp ./google.china.conf ./CHN.GOOG.txt
+cat ./apple.china.conf ./google.china.conf ./accelerated-domains.china.conf > ./CHN.ALL.txt
+sed -i "s|server=/||g" ./CHN*.txt && sed -i "s|/114.114.114.114||g" ./CHN*.txt
+cp ./accelerated-domains.china.conf ./CHN.conf && cp ./apple.china.conf ./CHN.AAPL.conf && cp ./google.china.conf ./CHN.GOOG.conf
+cat ./apple.china.conf ./google.china.conf ./accelerated-domains.china.conf > ./CHN.ALL.conf
+sed -i "s|server=/|DOMAIN-SUFFIX,|g" ./CHN*.conf && sed -i "s|/114.114.114.114||g" ./CHN*.conf
+cp ./accelerated-domains.china.conf ./CHN.yaml && cp ./apple.china.conf ./CHN.AAPL.yaml && cp ./google.china.conf ./CHN.GOOG.yaml
+cat ./apple.china.conf ./google.china.conf ./accelerated-domains.china.conf > ./CHN.ALL.yaml
+sed -i "s|server=/|  - '+.|g" ./CHN*.yaml && sed -i "s|/114.114.114.114|'|g" ./CHN*.yaml && sed -i "1s|^|payload:\n|" ./CHN*.yaml
+cp ./accelerated-domains.china.conf ./CHN.agh && cp ./apple.china.conf ./CHN.AAPL.agh && cp ./google.china.conf ./CHN.GOOG.agh
+cat ./apple.china.conf ./google.china.conf ./accelerated-domains.china.conf > ./CHN.ALL.agh
+sed -i "s|server=|[|g" ./CHN*.agh && sed -i "s|114.114.114.114|]tls://223.5.5.5|g" ./CHN*.agh
+rm ./accelerated-domains.china.conf ./apple.china.conf ./google.china.conf
 git init
 git add .
 updated=`date --rfc-3339 sec`
